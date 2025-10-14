3bb401b3333f443c5d10ce3a435919a554d231b7
Mauricio Olguin <mauolguisanchez@gmail.com>
2025-10-14 01:22:33 -0600

    Initial Commit

---
+++ Examen iOS/Examen_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Examen_iOSApp.swift
+//  Examen iOS
+//
+//  Created by Mauricio Olguín on 14/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Examen_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
